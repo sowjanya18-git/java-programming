2c298b3a94d12680dea0328752cc46912b4c6224
ssowji2019 <ssowji2019@gmail.com>
2020-04-24 11:01:23 +0800

    first java programming

---
+++ Helloworls.java
@@ -0,0 +1,9 @@
+
+public class Helloworls {
+
+    public static void (main string{) {
+           // prints "Hellow, worls" to the terminal window.
+           system .put.printin("Hellow, worls") 
+
+
+}
